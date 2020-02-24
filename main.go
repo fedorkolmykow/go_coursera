0f74f896cfb3312ed376b867031cbf3f86523bc8
Fedor <fedor@debian>
2020-02-24 22:33:27 +0300

    First commit

---
+++ main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"os"
+	"fmt"
+	"sort"
+	"io"
+)
+
+const (
+	leaf 		= "├───"
+	branch  	= "|\t"
+	lastleaf 	= "└───"
+	empty		= "\t"
+)
+
+func main() {
+	out := os.Stdout
+	if !(len(os.Args) == 2 || len(os.Args) == 3) {
+		panic("usage go run main.go . [-f]")
+	}
+	path := os.Args[1]
+	printFiles := len(os.Args) == 3 && os.Args[2] == "-f"
+	err := dirTree(out, path, printFiles)
+	if err != nil {
+		panic(err.Error())
+	}
+}
+
+func dirTree(out io.Writer, path string, printFiles bool)(err error){
+	openDir(out, path, "", printFiles)
+	return
+}
+
+func openDir(out io.Writer, path, shift string, printFiles bool)(err error){
+	branc := branch
+	lea := leaf
+	def_dir, err := os.Open(path)
+	defer def_dir.Close()
+	if err != nil {
+		return err
+	}
+	files, err:=def_dir.Readdir(0)
+	if err != nil {
+		return err
+	}
+	sort.Slice(files, func(i, j int) bool { return files[i].Name() < files[j].Name() })
+
+	for i, val:= range files{
+
+		if (i == len(files)-1) {
+			//fmt.Println(len(files))
+			branc = empty
+			lea = lastleaf
+		}
+
+		if val.IsDir() {
+			fmt.Fprintf(out, "%v%v\n", shift+lea, val.Name())
+			var nextPath = path + "/" + val.Name()
+			openDir(out, nextPath,shift + branc, printFiles)
+		} else if (printFiles) {
+			if val.Size() != 0 {
+				fmt.Fprintf(out, "%v%v (%db)\n", shift+lea, val.Name(), val.Size())
+			} else {
+				fmt.Fprintf(out, "%v%v (empty)\n", shift+lea, val.Name())
+			}
+		}
+	}
+	return
+}
